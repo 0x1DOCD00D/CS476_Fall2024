1b5528fc29e18d1499c1f6dceb3d3953dd7e8405
0x1DOCD00D <markgrechanik@gmail.com>
2024-08-29 11:53:19 -0500

    added the assignment op to the exp dsl

--- src/main/scala/ExpressionCalc.scala
+++ src/main/scala/ExpressionCalc.scala
@@ -1,9 +1,11 @@
-import ExpressionCalc.ExpOperation
-import ExpressionCalc.ExpOperation.Add
+import ExpressionCalc.ExpOperation.Assign
+
+import scala.collection.mutable
 
 object ExpressionCalc:
-  type EnvironmentTable = Map[String, Int]
-  given envTable: EnvironmentTable = Map("evenNumber" -> 2)
+  type EnvironmentTable = mutable.Map[String, Int]
+  type EnvironmentTableContext = EnvironmentTable ?=> Int
+  given envTable: EnvironmentTable = mutable.Map("evenNumber" -> 2)
 
   enum ExpOperation:
     case Value(i: Int)
@@ -11,13 +13,17 @@ object ExpressionCalc:
     case Add(p1: ExpOperation, p2: ExpOperation)
     case Mult(p1: ExpOperation, p2: ExpOperation)
     case Sub(p1: ExpOperation, p2: ExpOperation)
+    case Assign(variable: Variable, v: Value)
 
-  def eval(exp: ExpOperation): Int = exp match
+  def eval(exp: ExpOperation): EnvironmentTableContext = exp match
     case ExpOperation.Value(i) => i
     case ExpOperation.Variable(s) => summon[EnvironmentTable].getOrElse(s, throw new Exception("not found"))
     case ExpOperation.Add(p1, p2) => eval(p1) + eval(p2)
     case ExpOperation.Mult(p1, p2) => eval(p1) * eval(p2)
     case ExpOperation.Sub(p1, p2) => eval(p1) - eval(p2)
+    case Assign(v, i) =>
+      summon[EnvironmentTable] ++= mutable.Map(v.s -> i.i)
+      i.i
 
   def main(args: Array[String]): Unit = {
     import ExpOperation.*
@@ -29,6 +35,9 @@ object ExpressionCalc:
       Add(Mult(Value(2),Add(Value(3),Value(5))),Add(Value(5),Value(7)))
     )
     println(
-      eval(Add(Mult(Variable("evenNumber"), Add(Value(3), Value(5))), Sub(Value(5), Value(7))))
+      eval(Assign(Variable("x"), Value(5)))
+    )
+    println(
+      eval(Add(Mult(Variable("x"), Add(Value(3), Value(5))), Sub(Value(5), Value(7))))
     )
   }
