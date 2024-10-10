f779745790c0082354abbc0136852153fbb8b31c
0x1DOCD00D <markgrechanik@gmail.com>
2024-10-10 15:51:26 -0500

    corecursion 1

--- src/main/scala/Genericity.scala
+++ src/main/scala/Genericity.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.ListBuffer
+
 object Genericity:
 
   def id[TV](i: TV): TV = i
@@ -9,9 +11,22 @@ object Genericity:
 
 //  def id(i: Double): Double = i
 
+  def mymap[From, To](lst: List[From])(f: From => To): List[To] =
+    val len = lst.length
+    var index = 0
+    val dest = ListBuffer[To]()
+    while (index < len) {
+      dest += f(lst(index))
+      index = index + 1
+    }
+    dest.toList
+
   def main(args: Array[String]): Unit = {
     println(id[Int](5))
     println(id("abc"))
     println(idf)
     println(idf(4.2f))
+    println{
+      mymap[String, Double](List[String]("aa", "aaa", "aabb"))((s:String)=>s.length.toDouble)
+    }
   }
---
+++ src/main/scala/InfGenerator.scala
@@ -0,0 +1,13 @@
+object InfGenerator:
+//  data list ::= Nil | hd :: list
+// 1 :: (2 :: (3::Nil))
+  def inf(bc: Int): LazyList[Int] = bc #:: inf(bc+1)
+
+  def main(args: Array[String]): Unit = {
+    val ll = inf(1)
+    val ll1 = ll.take(10).toList
+    val ll2 = ll.drop(10).take(10).toList
+    println{ ll1
+    }
+    println(ll2)
+  }
